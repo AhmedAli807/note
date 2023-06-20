2d9c67d6d1389144889e234ae4777add623ac9cc
AhmedAli807 <f.20200138@gmail.com>
2023-06-20 15:01:06 +0300

    Create initFlutter & openBox

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,3 @@
 import 'dart:ui';
 const kPrimaryColor= Color(0xff62FCD7);
+const kNotesBox='notes_box';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:untitled11/constants.dart';
 import 'package:untitled11/views/notes_screen.dart';
 
-void main() {
+void main()async {
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
   runApp(const MyApp());
 }
 
